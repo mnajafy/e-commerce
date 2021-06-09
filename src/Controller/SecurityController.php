86f45826951833953bc1d61d5522de57015a1437
mnajafy <64306946+mnajafy@users.noreply.github.com>
2021-06-09 17:17:35 +0200

    Update SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,28 +2,70 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    /**
+     * @Route("/inscription", name="app_logup")
+     */
+    public function logup(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class,$user, [
+            'validation_groups' => ['Default', 'registration'],
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setRoles(['ROLE_ADMIN'])
+                ->setPassword($this->passwordEncoder->encodePassword($user, $user->getPassword()))
+            ;
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'Vous etes bien inscrie connectez vous!');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/logup.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('admin_index');
+        if ($user = $this->getUser()) {
+            return $this->redirectToRoute('profile', [
+                'username' => $user->getUsername(), 
+                'id' => $user->getId()
+            ]);
         }
-
-        // get the login error if there is one
+        
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
+        
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername, 
+            'error' => $error
+        ]);
     }
 
     /**
